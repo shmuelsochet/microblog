43e99b994c3b4c6ac209491653fe36baa904f6d6
ssochet <sysochet@gmail.com>
2018-03-22 16:36:11 -0400

    [Bug]Remove function call from decorator.

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
-@login.user_loader()
+@login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
