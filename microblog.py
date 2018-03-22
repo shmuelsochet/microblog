8d4cafc0717d07c4353fb4fdfa96e894e374d3a5
ssochet <sysochet@gmail.com>
2018-03-22 10:51:39 -0400

    [4.10]Shell context, flask shell.

--- microblog.py
+++ microblog.py
@@ -1,4 +1,15 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Post': Post
+    }
+
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
