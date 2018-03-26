b9c8c6fe82340108fe16f51bf8b3a9dd203e7844
ssochet <sysochet@gmail.com>
2018-03-26 13:28:06 -0400

    [5.9]User registration, add .first() to fix bug EqualTo for confirm_password

--- app/forms.py
+++ app/forms.py
@@ -13,16 +13,16 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[
-        DataRequired()])
+        DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data)
+        user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Username is already taken, choose another')
 
     def validate_email(self, email):
-        email = User.query.filter_by(email=email.data)
+        email = User.query.filter_by(email=email.data).first()
         if email is not None:
             raise ValidationError('Please use a different email address')
 
