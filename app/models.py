acc67823007f9875e88dd03a096061d355af23d3
ssochet <sysochet@gmail.com>
2018-04-11 10:24:44 -0400

    [Bug]Remove nullable false from password_hash field

--- app/models.py
+++ app/models.py
@@ -23,7 +23,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
