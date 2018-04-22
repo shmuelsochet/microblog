7f6483a7b4fc0f2f7ede2bdd8cc305f96be1c473
ssochet <sysochet@gmail.com>
2018-04-21 22:52:18 -0400

    [10.6]Sending a password reset email

--- app/email.py
+++ app/email.py
@@ -1,10 +1,25 @@
 from flask_mail import Message
-from app import mail
+from flask import render_template
+from app import mail, app
 
 
-def send_mail(subject, sender, recipients, cc, bcc, text_body, html_body):
+def send_email(subject, sender, recipients, cc, bcc, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients, cc=cc,
                   bcc=bcc)
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
+
+
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+    send_email('[Microblog] Reset your password',
+               sender=app.config['ADMINS'][0],
+               recipients=[user.email],
+               cc=app.config['ADMINS'][0],
+               bcc=app.config['ADMINS'][0],
+               text_body=render_template('email/reset_password.txt',
+                                         user=user, token=token),
+               html_body=render_template('email/reset_password.html',
+                                         user=user, token=token)
+               )
--- app/models.py
+++ app/models.py
@@ -76,14 +76,14 @@ class User(UserMixin, db.Model):
             app.config['SECRET_KEY'],algorithm='HS256'
         ).decode('utf-8')
 
-    @staticmethod
-    def verify_reset_password_token(token):
-        try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
-                            algorithms=['HS256'])['reset_password']
-        except:
-            return
-        return User.query.get(id)
+
+def verify_reset_password_token(token):
+    try:
+        id = jwt.decode(token, app.config['SECRET_KEY'],
+                        algorithms=['HS256'])['reset_password']
+    except:
+        return
+    return User.query.get(id)
 
 
 class Post(db.Model):
