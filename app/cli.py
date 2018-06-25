f4ef1c7c69091f6e8baed5c68d79a73f6b8b4c66
shmuelsochet <sysochet@gmail.com>
2018-06-25 09:52:14 -0400

    [13.6]Formatting

--- app/cli.py
+++ app/cli.py
@@ -10,6 +10,18 @@ def translate():
 
 
 @translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initializes a new language. """
+    if os.system('pybabel extract -F babel.cfg -k _1 -o messages.pot  .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel init -i messages.pot -d app/translations -l ' +
+                 lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
+
+
+@translate.command()
 def update():
     """Update all languages. """
     if os.system('pybabel extract -F babel.cfg -k _1 -o messages.pot .'):
@@ -24,15 +36,3 @@ def compile():
     """Compile all languages. """
     if os.system('pybabel compile -d app/translations'):
         raise RuntimeError('compile command failed')
-
-
-@translate.command()
-@click.argument('lang')
-def init(lang):
-    """Initializes a new language. """
-    if os.system('pybabel extract -F babel.cfg -k _1 -o messages.pot  .'):
-        raise RuntimeError('extract command failed')
-    if os.system('pybabel init -i messages.pot -d app/translations -l ' +
-                 lang):
-        raise RuntimeError('init command failed')
-    os.remove('messages.pot')
