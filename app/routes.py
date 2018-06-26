70d6549178a85477cef1db22a809e1f11a3bbf7d
ssochet <sysochet@gmail.com>
2018-06-25 21:24:39 -0400

    14.3]Language identification

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+from guess_language import guess_language
 from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import (
@@ -32,7 +34,10 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
